97a30902e02ac48577b3fc725182b04fd6ad8345
Bugi Pradana Nugroho <buginugroho17@gmail.com>
2024-03-13 00:15:11 +0700

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import ProductPage from './pages/ProductPage'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<ProductPage />}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App
---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,11 @@
+function ProductPage() {
+    return (
+        <>
+            <h1>Hello from ProductPage</h1>
+
+            <h1>Sphinx of black quartz, judge my vow!</h1>
+        </>
+    );
+}
+
+export default ProductPage;
