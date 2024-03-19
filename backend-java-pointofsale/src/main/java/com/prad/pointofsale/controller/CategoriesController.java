bbc3c07be9b28ce249ced4033da867f2475848cb
Bugi Pradana Nugroho <buginugroho17@gmail.com>
2024-03-19 16:43:00 +0700

    add CORS header policy

--- backend-java-pointofsale/src/main/java/com/prad/pointofsale/controller/CategoriesController.java
+++ backend-java-pointofsale/src/main/java/com/prad/pointofsale/controller/CategoriesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.prad.pointofsale.model.response.CategoriesResponse;
 import com.prad.pointofsale.model.response.MyHttpResponse;
 import com.prad.pointofsale.service.CategoriesService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/pos/api")
 public class CategoriesController {
